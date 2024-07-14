5ea880888ad8f89e83e8fc5e6b411217d142ebfa
jarek7410 <39216951+jarek7410@users.noreply.github.com>
2024-07-14 13:44:17 +0200

    #2

--- screens/befor_login/RegisterScreen.tsx
+++ screens/befor_login/RegisterScreen.tsx
@@ -1,21 +1,67 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { screen } from '../../enum/screen.ts';
-import { RootStackParamList } from '../rootStats.ts';
+import React, { useRef } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { screen } from "../../enum/screen.ts";
+import { RootStackParamList } from "../rootStats.ts";
+import { LoginRegisterStyle } from "../../styles/loginRegisterStyle.ts";
+import { Colors } from "../../styles/Colors.ts";
 
 type Props = NativeStackScreenProps<RootStackParamList, screen.Register>;
 
 export const RegisterScreen = ({ navigation }: Props) => {
+  const passwordInput = useRef<TextInput>(null);
+  const emailInput = useRef<TextInput>(null);
+  const [username, setUsername] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  const [email, setEmail] = React.useState<string>("");
+  const register = async () => {
+    console.log("register: " + username + " " + email);
+    navigation.goBack();
+  };
   return (
-    <View>
+    <View style={{ flex: 1, backgroundColor: Colors.background }}>
       <Text>Register Screen</Text>
       <Button
-        title={'register'}
+        title={"go back"}
         onPress={() => {
           navigation.goBack();
         }}
       />
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <TextInput
+          style={LoginRegisterStyle.TextInput}
+          value={username}
+          onChangeText={setUsername}
+          placeholder={"username"}
+          autoFocus={true}
+          returnKeyType="next"
+          onSubmitEditing={() => emailInput.current?.focus()}
+          blurOnSubmit={false}
+        />
+        <TextInput
+          style={LoginRegisterStyle.TextInput}
+          placeholder={"email"}
+          onChangeText={setEmail}
+          value={email}
+          // secureTextEntry
+          ref={emailInput}
+          returnKeyType="next"
+          onSubmitEditing={() => passwordInput.current?.focus()}
+          blurOnSubmit={false}
+        />
+
+        <TextInput
+          style={LoginRegisterStyle.TextInput}
+          placeholder={"password"}
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+          ref={passwordInput}
+          blurOnSubmit={true}
+          returnKeyType="send"
+        />
+        <Button title={"register"} onPress={register} />
+      </View>
     </View>
   );
 };
